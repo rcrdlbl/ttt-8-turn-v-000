b457606104d2e273d41f56338bcddb58714dea01
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-18 21:03:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,18 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move? (board, index)
+  if (position_taken?(board, index) == false) && index >= 0 && index <= 8
+    return true
+  end
+end
+
+def position_taken? (board, index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else
+    return true
+  end
+
+end
